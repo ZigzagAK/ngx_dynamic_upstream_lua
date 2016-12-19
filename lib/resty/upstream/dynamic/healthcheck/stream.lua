5ae37446bb51ca010cc2eb85ab0bcbe853f3d1b5
Aleksey Konovkin <alkon2000@mail.ru>
2016-12-20 00:07:13 +0300

    fix upstream_type

--- lib/resty/upstream/dynamic/healthcheck/stream.lua
+++ lib/resty/upstream/dynamic/healthcheck/stream.lua
@@ -7,20 +7,20 @@ local common = require "resty.upstream.dynamic.healthcheck.common"
 local upstream = require "ngx.dynamic_upstream.stream"
 
 function _M.new(opts)
-    local r = common.new("http", opts, { set_peer_down = _M.set_peer_down,
-                                         set_peer_up   = _M.set_peer_up,
-                                         get_peers     = _M.get_peers,
-                                         get_upstreams = _M.get_upstreams
-                                       })
+    local r = common.new("stream", opts, { set_peer_down = _M.set_peer_down,
+                                           set_peer_up   = _M.set_peer_up,
+                                           get_peers     = _M.get_peers,
+                                           get_upstreams = _M.get_upstreams
+                                         })
     return r
 end
 
 function _M.successes(dict, upstream, peer)
-    return common.successes("http", dict, upstream, peer)
+    return common.successes("stream", dict, upstream, peer)
 end
 
 function _M.fails(dict, upstream, peer)
-    return common.fails("http", dict, upstream, peer)
+    return common.fails("stream", dict, upstream, peer)
 end
 
 --- Wrappers -----------------------------------------------------------------------
