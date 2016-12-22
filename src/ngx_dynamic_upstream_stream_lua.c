66662eb96c0157b2c4de645787b2ee1a980aeab6
Aleksey Konovkin <alkon2000@mail.ru>
2016-12-22 18:25:22 +0300

    fix

--- src/ngx_dynamic_upstream_stream_lua.c
+++ src/ngx_dynamic_upstream_stream_lua.c
@@ -422,7 +422,11 @@ ngx_stream_dynamic_upstream_lua_get_healthcheck(lua_State * L)
         lua_pushlstring(L, (char *) uscf->host.data, uscf->host.len);
         lua_rawset(L, -3);
 
-        ngx_stream_dynamic_upstream_lua_push_healthcheck(L, uscf);
+        if (uscf->srv_conf != NULL) {
+            ngx_stream_dynamic_upstream_lua_push_healthcheck(L, uscf);
+        } else {
+            lua_pushnil(L);
+        }
 
         lua_rawseti(L, -2, i + 1);
     }
